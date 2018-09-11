3a4b18327ad3efdc110b6392129c37c4b80ff2fe
BuildTools <unconfigured@null.spigotmc.org>
2018-09-11 03:50:46 -0400

    \

--- main.java
+++ main.java
@@ -3,7 +3,10 @@ class Main {
     public static void Main(String[] args){
         // scans for user input
         Scanner x = new Scanner(System.in);
-        Scanner y =
+        Scanner y = new Scanner(System.in);
+        // prints x var 
+        System.out.Println("insert x value");
+        int num1 = x.nextInt
 
     }
 }
