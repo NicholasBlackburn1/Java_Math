e25eb0d425a99656a37878724bf397723c8dee85
BuildTools <unconfigured@null.spigotmc.org>
2018-09-11 05:11:45 -0400

    Ca

---
+++ add.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        // scans for user input
+        int x, y ;
+        Scanner in = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
+        // prints  and reads x var 
+        System.out.println("insert x value");
+        x = in.nextInt();
+        // prints and reads y var 
+        System.out.println("insert y value");
+        y = in2.nextInt();
+       // number
+
+       
+        double result = x + y;
+        //  Prints results 
+         System.out.println(result);
+       
+    }
+}
