f7d21c031202f0362c70786a8bd17979ff10e3ed
BuildTools <unconfigured@null.spigotmc.org>
2018-09-11 05:04:41 -0400

    annio

--- main.java
+++ main.java
@@ -5,12 +5,13 @@ class Main {
         // scans for user input
         int x1, y1 ;
         Scanner in = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
         // prints  and reads x var 
         System.out.println("insert x value");
-        in.nextInt();
+        x = in.nextInt();
         // prints and reads y var 
         System.out.println("insert y value");
-        in.nextInt();
+        y = in2.nextInt();
        // number
 
         int n1 = Integer.parseInt(x);
