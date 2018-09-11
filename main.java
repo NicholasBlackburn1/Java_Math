87b7a48370a501ab54c43e5c3b49858d556dcff7
BuildTools <unconfigured@null.spigotmc.org>
2018-09-11 05:25:20 -0400

    add

--- main.java
+++ main.java
@@ -1,23 +1,27 @@
-import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args){
-        // scans for user input
-        int x, y ;
-        Scanner in = new Scanner(System.in);
-        Scanner in2 = new Scanner(System.in);
-        // prints  and reads x var 
-        System.out.println("insert x value");
-        x = in.nextInt();
-        // prints and reads y var 
-        System.out.println("insert y value");
-        y = in2.nextInt();
-       // number
+class Main{
+    public static void main(String[] args) {
+    Data data = new Data();
+    data.menu();
 
-       
-        double result = x + y;
-        //  Prints results 
-         System.out.println(result);
-       
+    Scanner scanner = new Scanner(System.in);
+    int choice = scanner.nextInt();
+
+    switch (choice) {
+        case 1:
+             add();
+            break;
+        case 2:
+            // Perform "encrypt number" case.
+            break;
+        case 3:
+            // Perform "decrypt number" case.
+            break;
+        case 4:
+            // Perform "quit" case.
+            break;
+        default:
+            // The user input an unexpected choice.
     }
+}
 }
