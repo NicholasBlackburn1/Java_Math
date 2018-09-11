b10a724ef332b0d1c3ce027e681d99cf71204861
BuildTools <unconfigured@null.spigotmc.org>
2018-09-11 07:45:51 -0400

    s

--- main.java
+++ main.java
@@ -1,21 +1,19 @@
-
+import com.googlecode.lanterna
 class Main{
     public static void main(String[] args) {
-        int selection;
-        Scanner input = new Scanner(System.in);
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+        Terminal terminal = null;
+try {
+    terminal = defaultTerminalFactory.createTerminal();
+    terminal.putCharacter('H');
+    terminal.putCharacter('e');
+    terminal.putCharacter('l');
+    terminal.putCharacter('l');
+    terminal.putCharacter('o');
+    terminal.putCharacter('\n');
+    terminal.flush();
 
-        /***************************************************/
-
-        System.out.println("Nicks Calculator");
-        System.out.println("-------------------------\n");
-        System.out.println("1 - Add ");
-        System.out.println("2 - subtract");
-        System.out.println("3 - muilply");
-        System.out.println("4 - divied");
-        System.out.println("5 - Quitz")
-
-        selection = input.nextInt();
-        return selection;    
-    }
 }
+finally{}
+    }
 }
