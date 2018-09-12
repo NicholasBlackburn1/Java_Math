b030be2283873c473652c2b2dd47d4522db35855
BuildTools <unconfigured@null.spigotmc.org>
2018-09-12 03:34:57 -0400

    added reas

--- main.java
+++ main.java
@@ -1,19 +1,4 @@
-import com.googlecode.lanterna.*;
 class Main{
     public static void main(String[] args) {
-        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
-        Terminal terminal = null;
-try {
-    terminal = defaultTerminalFactory.createTerminal();
-    terminal.putCharacter('H');
-    terminal.putCharacter('e');
-    terminal.putCharacter('l');
-    terminal.putCharacter('l');
-    terminal.putCharacter('o');
-    terminal.putCharacter('\n');
-    terminal.flush();
-
-}
-finally{}
-    }
+    }    
 }
