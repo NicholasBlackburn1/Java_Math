9fed17082cb816d94024b15c2a04c8b343427c92
BuildTools <unconfigured@null.spigotmc.org>
2018-09-15 10:45:52 -0400

    edd

--- main.java
+++ main.java
@@ -1,18 +1,25 @@
 
 import java.util.Scanner;
-
-import com.sun.org.apache.xpath.internal.operations.Variable;
-
 class Main{
     public static void main(String[] args)  {
         // scans for input 
         Scanner userinput = new Scanner(System.in);
+        
         // prints menu 
            System.out.println("JAVA_MATH by NichoalsBlackburn");
            System.out.println("");
            System.out.println("pick options 1-4");
            System.out.println("1 - Add");
            System.out.println("2 - Subtract");
+           System.out.println("3 - Multiply");
+           System.out.Println("4 - Divided");
+           System.out.println("5 - Exit");
+           x = userinput.nextInt();
+           // options 
+          if (x = 1) {
+            System.out.print("workd");
+          } 
+
+          }
    
-    }    
-}
+    }
